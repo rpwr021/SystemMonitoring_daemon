deacf9982c5c87d39184913627c8fb467b8aafd3
rpwr021 <rpawar.021@gmail.com>
2012-12-29 21:28:46 +0530

    Update sys_dmn.sh

--- sys_dmn.sh
+++ sys_dmn.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ####
 ##service enabler for systat daemon 
-##auth rpawar@amdocs.com
+##auth rpawar.021@gmail.com
 ####
 
 
 . /etc/rc.d/init.d/functions
 
-export lockfile=/tmp/lock/subsys/pil_systat
-export STARTPIDFILE=/tmp/lock/subsys/logs/pidpil
+export lockfile=/tmp/systat/smlock
+export STARTPIDFILE=/tmp/systat/pidf
 export RETVAL=0
 #export STIME=1800
 
@@ -21,12 +21,12 @@ start() {
 		if [ -z $PID ] ; then 
 				RETVAL=1
                 PID=`cat $STARTPIDFILE`
-                echo pil_systat already running: $PID
+                echo systat already running: $PID
 		fi		
                 exit 2; 
         else 
 						/bin/touch "$lockfile"  "$STARTPIDFILE" > /dev/null 2>&1
-						nohup /pil_user2/pil/users/piltools/SysStat/pil_systat service  > /dev/null 2>&1 &
+						nohup ${RHOME}/SysStat/pil_systat service  > /dev/null 2>&1 &
 						PID=$!
 						echo $"pil_systat Started : PID is $PID" 
 						RETVAL=$?
@@ -47,16 +47,14 @@ stopx() {
                 echo $"Stopping pil_systat  [OK]."
                 killproc -p $STARTPIDFILE /pil_user2/pil/users/piltools/SysStat/pil_systat
 				/bin/rm "$lockfile"  "$STARTPIDFILE" > /dev/null 2>&1
-				kill -9 `ps -ef  | grep -vE grep | grep pilsys_dmn | cut -d " " -f2`      >/dev/null 2>&1
-				kill -9 `ps -ef  | grep -vE grep | grep pil_systat | cut -d " " -f2`      >/dev/null 2>&1
+				kill -9 `ps -ef  | grep -vE grep | grep sys_dmn | cut -d " " -f2`      >/dev/null 2>&1
+				kill -9 `ps -ef  | grep -vE grep | grep systat | cut -d " " -f2`      >/dev/null 2>&1
 				kill -9 `ps -ef  | grep -vE grep | grep "sleep 1843" | cut -d " " -f2`  >/dev/null 2>&1
                 echo "done!"
                 rm -f $STARTPIDFILE
 				
 	fi			
 	
-	#killproc pil_systat
-	#kill `ps -ef | grep pil_systat | grep -vE grep | cut -d " " -f2`
 	
 	
 }
@@ -79,10 +77,10 @@ case "$1" in
         ;;
   status)
 	if [ -f $lockfile ]; then
-		echo $"pil_systat is enabled. Running with PID `cat $STARTPIDFILE` "
+		echo "systat is enabled. Running with PID `cat $STARTPIDFILE` "
 		RETVAL=0
 	else
-		echo $"PIL_systat is not Running."
+		echo "Systat is not Running."
 		RETVAL=3
 	fi
 	;;
